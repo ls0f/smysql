fa70a53b072101d9130b362c1eb0d84de4bdc488
lovedboy <lovedboy.tk@qq.com>
2015-08-29 18:27:40 +0800

    update readme.md

--- smysql/test.py
+++ smysql/test.py
@@ -1,27 +1,9 @@
-#coding:utf-8
-
-from simple_mysql import DB, DBConfig
+from smysql import DB, DBConfig
 
 DBConfig.host = '192.168.1.150'
+DBConfig.passwd = ''
+DBConfig.user = 'root'
+DBConfig.charset = 'utf8'
 
 db = DB("test_db")
-
-# xiaoming = db.insert('user', obj_dict={"name": "小明", "coin": 0})
-# xiaohong = db.insert('user', obj_dict={"name": "小红", "coin": 0})
-
-
-# db.update('user', query_dict={"uid": 6}, update_dict={'coin': DB.field("coin+50")})
-
-# print db.query('user', query_dict={"uid": 5})[0]['coin']
-
-
-# for item in db.query('user', query_dict={"coin__gt": 10}):
-    # print item['name']
-
-# db.delete("user", query_dict={"uid": 1})
-
-db = DB("test_db", auto_trans=False)
-db.update("user", query_dict={"uid": 6}, update_dict={"coin": DB.field("coin+100")},sql_row_count=1)
-db.update("user", query_dict={"uid": 5, 'coin__gte': 100}, update_dict={"coin": DB.field("coin-100")}, sql_row_count=1)
-db.end_trans()
-
+db.query('user')
